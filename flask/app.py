abfc28604d25d788a2810f7c9cc573175e2c65cb
André Silva do Nascimento <44279542+andre20022@users.noreply.github.com>
2024-11-06 15:07:53 -0300

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -29,6 +29,9 @@ def upload_image():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detectar faces
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1, minSize=(30, 30))
+    if len(faces) == 0:
+        return jsonify({'error': 'No faces'}), 400
+    
     # Desenhar retângulos ao redor das faces
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -38,6 +41,44 @@ def upload_image():
     img_bytes = img_encoded.tobytes()
 
     return send_file(BytesIO(img_bytes), mimetype='image/png')
+
+@app.route('/comparar-rostos', methods=['POST'])
+def comparar_rostos():
+    # Verificar se as imagens foram enviadas
+    if 'filea' not in request.files or 'fileb' not in request.files:
+        return jsonify({"error": "Por favor, envie duas imagens para comparação."}), 400
+
+    # Obter as imagens enviadas
+    image1_file = request.files['filea']
+    image2_file = request.files['fileb']
+
+    # Converter as imagens para arrays NumPy
+    image1 = np.asarray(bytearray(image1_file.read()), dtype=np.uint8)
+    image2 = np.asarray(bytearray(image2_file.read()), dtype=np.uint8)
+
+    # Decodificar as imagens
+    image1 = cv2.imdecode(image1, cv2.IMREAD_GRAYSCALE)
+    image2 = cv2.imdecode(image2, cv2.IMREAD_GRAYSCALE)
+
+    # Calcular os histogramas
+    hist1 = cv2.calcHist([image1], [0], None, [256], [0, 256])
+    hist2 = cv2.calcHist([image2], [0], None, [256], [0, 256])
+
+    # Normalizar os histogramas
+    cv2.normalize(hist1, hist1)
+    cv2.normalize(hist2, hist2)
+
+    # Comparar os histogramas
+    similarity = cv2.compareHist(hist1, hist2, cv2.HISTCMP_CORREL)
+
+    # Definir limite de similaridade
+    if similarity > 0.8:
+        result = "As imagens são semelhantes."
+    else:
+        result = "As imagens são diferentes."
+
+    return jsonify({"similaridade": similarity, "resultado": result})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
