da8fed54695d92cfac02cd5c04e01f20081fb26e
André Silva do Nascimento <44279542+andre20022@users.noreply.github.com>
2024-11-06 13:45:35 -0300

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -25,24 +25,19 @@ def upload_image():
     # Converter imagem para o formato que o OpenCV pode processar
     img = Image.open(file.stream)
     img = np.array(img)
-
     # Converter para escala de cinza
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     # Detectar faces
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    print(faces);
-
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1, minSize=(30, 30))
     # Desenhar retângulos ao redor das faces
-    # for (x, y, w, h) in faces:
-    #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    # # Converter a imagem com as faces marcadas de volta para um formato que pode ser enviado no Flask
-    # _, img_encoded = cv2.imencode('.png', img)
-    # img_bytes = img_encoded.tobytes()
+    # Converter a imagem com as faces marcadas de volta para um formato que pode ser enviado no Flask
+    _, img_encoded = cv2.imencode('.png', img)
+    img_bytes = img_encoded.tobytes()
 
-    # return send_file(BytesIO(img_bytes), mimetype='image/png')
+    return send_file(BytesIO(img_bytes), mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
